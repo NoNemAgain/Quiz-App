a51b7c1ed90b24dbb7bd5107c2ed6ea308c40197
NoNemAgain <57526481+NoNemAgain@users.noreply.github.com>
2022-05-13 15:06:31 +0200

    [Fini ] 4 - Mise en place de l’authentification
    End 4 -

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import jwt_utils
 
 app = Flask(__name__)
 
@@ -10,6 +11,15 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+@app.route('/login', methods=['POST'])
+def login():
+    payload = request.get_json()
+    
+    if payload['password'] == "Vive l'ESIEE !" :
+        return jsonify({'token':jwt_utils.build_token()})
+    else :
+        return '', 401
 
 if __name__ == "__main__":
     app.run(ssl_context='adhoc')
