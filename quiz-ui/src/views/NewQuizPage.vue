37a1eee980eb9a0d98323b9581a71b3df854fe61
NoNemAgain <57526481+NoNemAgain@users.noreply.github.com>
2022-05-31 14:02:16 +0200

    Composant : NewQuizPage

---
+++ quiz-ui/src/views/NewQuizPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>Saisissez votre nom :</p>
+    <input type="text" class="form-control" v-model="username">
+    <button class="btn btn-outline-danger" @click="launchNewQuiz">GO !</button>
+  </div>
+</template>
+
+<script>
+import participationStorageService from "@/services/ParticipationStorageService";
+// import quizApiService from "@/services/quizApiService";
+
+export default {
+  name: "NewQuizPage",
+  data() {
+    const username = participationStorageService.getPlayerName();
+    return {
+      username : username
+    };
+  },
+  async created() {
+
+  },
+  methods:{
+    launchNewQuiz(){
+      participationStorageService.savePlayerName(this.username);
+      this.$router.push('/questions');
+    },
+  }
+};
+</script>
